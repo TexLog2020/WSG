1a3376caea6a29c99bf4d186c8d7e0bb813dbb45
Houston <bboyliakos@gmail.com>
2020-11-23 22:33:02 +0200

    Button23

--- app/src/main/java/com/example/wsg/MainActivity.java
+++ app/src/main/java/com/example/wsg/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    int button23;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
