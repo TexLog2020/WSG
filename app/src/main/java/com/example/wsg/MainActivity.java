cfe2e643dbaf9a88ca6345706014e2221832ff21
George <dbsk400@gmail.com>
2020-11-25 14:08:33 +0200

    Update Login Procedure with Register changes Commit

--- app/src/main/java/com/example/wsg/MainActivity.java
+++ app/src/main/java/com/example/wsg/MainActivity.java
@@ -1,17 +1,30 @@
 package com.example.wsg;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    int button;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public TextView register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        register = (TextView) findViewById(R.id.register);
+        register.setOnClickListener(this);
     }
 
-    int k;
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.register:
+                startActivity(new Intent(this,RegisterUser.class));
+                break;
+        }
+    }
 }
