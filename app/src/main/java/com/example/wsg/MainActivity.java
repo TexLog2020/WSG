c51a3c75439f1de5a44accfcbfc6b758113f4e4f
Sando96 <sndhsndh20@gmail.com>
2020-12-02 00:02:44 +0200

    AvailableHoursCommit2

--- app/src/main/java/com/example/wsg/MainActivity.java
+++ app/src/main/java/com/example/wsg/MainActivity.java
@@ -1,102 +1,39 @@
 package com.example.wsg;
 
-import androidx.annotation.NonNull;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
-    private TextView register;
-    private EditText editTextemail,editTextpassword;
-    private Button signIn;
 
-    private FirebaseAuth mAuth;
-    private ProgressBar progressBar;
+
+    private EditText number;
+    private Button submitButton;
+    private FirebaseDatabase db = FirebaseDatabase.getInstance();
+    private DatabaseReference root = db.getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        register = (TextView) findViewById(R.id.register);
-        register.setOnClickListener(this);
+        number = findViewById(R.id.TextNumber);
 
-        signIn = (Button) findViewById(R.id.signIn);
-        signIn.setOnClickListener(this);
-
-        editTextemail = (EditText) findViewById(R.id.email);
-        editTextpassword = (EditText)findViewById(R.id.password);
-
-        progressBar =(ProgressBar) findViewById (R.id.progressBar);
-
-        mAuth = FirebaseAuth.getInstance();
-
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.register:
-                startActivity(new Intent(this,RegisterUser.class));
-                break;
-            case R.id.signIn:
-                userLogin();
-                break;
-        }
-    }
-
-    private void userLogin() {
-        String email = editTextemail.getText().toString().trim();
-        String password = editTextpassword.getText().toString().trim();
-
-        if(email.isEmpty()){
-            editTextemail.setError("Email Is Required");
-            editTextemail.requestFocus();
-            return;
-        }
-
-        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
-            editTextemail.setError("Please Enter a Valid Email");
-            editTextemail.requestFocus();
-            return;
-
-        }
-
-        if(password.isEmpty()){
-            editTextpassword.setError("Min Password Length is 6 Characters");
-            editTextpassword.requestFocus();
-            return;
-        }
-
-        progressBar.setVisibility(View.VISIBLE);
-
-        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+        submitButton = (Button) findViewById(R.id.submitButton);
+        submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-
-                if(task.isSuccessful()) {
-                    startActivity(new Intent(MainActivity.this,ProfileActivity.class));
-                }else{
-                    Toast.makeText(MainActivity.this,"Failed To Login! Failed Check Your Credentials",Toast.LENGTH_LONG).show();
-                }
+            public void onClick(View v) {
+                String TextNumber = number.getText().toString();
+                root.setValue(TextNumber);
             }
         });
-
-
-
     }
 }
