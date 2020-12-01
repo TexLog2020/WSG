1412ef3bfadbfbd13018c03728a544764bdf6256
symetsak <tsakanias.simos@gmail.com>
2020-12-01 21:43:26 +0200

    deleteEmployeeCommitNeedChanges

--- app/src/main/java/com/example/wsg/MainActivity.java
+++ app/src/main/java/com/example/wsg/MainActivity.java
@@ -1,16 +1,45 @@
-package com.example.wsg;
+package com.example.firebasedatabasesimos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.net.IDN;
 
 public class MainActivity extends AppCompatActivity {
+
+    final DatabaseReference drEmployees = FirebaseDatabase.getInstance().getReference("Employees");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button deleteButton = (Button) findViewById(R.id.deleteButton);
+
+        String ID = "1";
+
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                deleteEmployee(ID);
+            }
+        });
+
     }
 
-    int k;
+    private void deleteEmployee(String id) {
+
+        drEmployees.child(id);
+
+        drEmployees.removeValue();
+
+        Toast.makeText(this,"The employeer is deleted...",Toast.LENGTH_LONG).show();
+    }
 }
