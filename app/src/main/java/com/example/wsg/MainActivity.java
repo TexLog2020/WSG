964be24616485d52264e32da51490174d21ff3f3
Sando96 <sndhsndh20@gmail.com>
2020-12-02 00:02:44 +0200

    AvailableHoursCommit2

--- app/src/main/java/com/example/wsg/MainActivity.java
+++ app/src/main/java/com/example/wsg/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.wsg;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+    private EditText number;
+    private Button submitButton;
+    private FirebaseDatabase db = FirebaseDatabase.getInstance();
+    private DatabaseReference root = db.getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        number = findViewById(R.id.TextNumber);
+
+        submitButton = (Button) findViewById(R.id.submitButton);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String TextNumber = number.getText().toString();
+                root.setValue(TextNumber);
+            }
+        });
     }
 }
