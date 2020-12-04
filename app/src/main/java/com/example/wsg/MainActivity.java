c802cacb91f36f968676d2a7ddadf50c033e6a86
Houston <bboyliakos@gmail.com>
2020-12-04 23:35:58 +0200

    Repair Of SignUp MainActivity

--- app/src/main/java/com/example/wsg/MainActivity.java
+++ app/src/main/java/com/example/wsg/MainActivity.java
@@ -1,15 +1,102 @@
 package com.example.wsg;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    int button;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView register;
+    private EditText editTextemail,editTextpassword;
+    private Button signIn;
+
+    private FirebaseAuth mAuth;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        register = (TextView) findViewById(R.id.register);
+        register.setOnClickListener(this);
+
+        signIn = (Button) findViewById(R.id.signIn);
+        signIn.setOnClickListener(this);
+
+        editTextemail = (EditText) findViewById(R.id.email);
+        editTextpassword = (EditText)findViewById(R.id.password);
+
+        progressBar =(ProgressBar) findViewById (R.id.progressBar);
+
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.register:
+                startActivity(new Intent(this,RegisterUser.class));
+                break;
+            case R.id.signIn:
+                userLogin();
+                break;
+        }
+    }
+
+    private void userLogin() {
+        String email = editTextemail.getText().toString().trim();
+        String password = editTextpassword.getText().toString().trim();
+
+        if(email.isEmpty()){
+            editTextemail.setError("Email Is Required");
+            editTextemail.requestFocus();
+            return;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            editTextemail.setError("Please Enter a Valid Email");
+            editTextemail.requestFocus();
+            return;
+
+        }
+
+        if(password.isEmpty()){
+            editTextpassword.setError("Min Password Length is 6 Characters");
+            editTextpassword.requestFocus();
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if(task.isSuccessful()) {
+                    startActivity(new Intent(MainActivity.this,ProfileActivity.class));
+                }else{
+                    Toast.makeText(MainActivity.this,"Failed To Login! Failed Check Your Credentials",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+
+
     }
 }
--- app/src/main/java/com/example/wsg/ProfileActivity.java
+++ app/src/main/java/com/example/wsg/ProfileActivity.java
@@ -1,6 +1,5 @@
 package com.example.wsg;
 
-package com.example.loginsignup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
