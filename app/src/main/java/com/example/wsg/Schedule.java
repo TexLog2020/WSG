637801f933a8f4c61b2ad37a1050a6c7e870bc10
Sando96 <sndhsndh20@gmail.com>
2021-01-04 17:55:24 +0200

    ScheduleUpdate

--- app/src/main/java/com/example/wsg/Schedule.java
+++ app/src/main/java/com/example/wsg/Schedule.java
@@ -6,7 +6,9 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.wsg.helpers.DaySchedule;
 import com.example.wsg.helpers.DaysOfTheWeek;
+import com.example.wsg.helpers.Employee;
 import com.example.wsg.helpers.ScheduleHelper;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -30,9 +32,9 @@ import java.util.Map;
 @SuppressWarnings("java:S110")
 public class Schedule extends AppCompatActivity {
 
-    private static final int NUMBER_OF_WEEKS = 4;
-    private static final int PEOPLE_ON_MORNING_SHIFT = 3;
-    private static final int PEOPLE_ON_AFTERNOON_SHIFT = 2;
+    public static final int NUMBER_OF_WEEKS = 2;
+    private static final int PEOPLE_ON_MORNING_SHIFT = 2;
+    private static final int PEOPLE_ON_AFTERNOON_SHIFT = 3;
     private static final int PEOPLE_ON_NIGHT_SHIFT = 1;
     private static final String TABLE_SCHEDULE = "Schedule";
     private static final String WEEK_WEEK = "Weeks/Week";
@@ -42,12 +44,28 @@ public class Schedule extends AppCompatActivity {
     private static final String NIGHT_SHIFT = "nightShift";
 
     @Override
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
 
         FirebaseDatabase db = FirebaseDatabase.getInstance();
+
+        Query cleanupQuery = db.getReference(TABLE_SCHEDULE).child("Weeks");
+        cleanupQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    for (DataSnapshot week : dataSnapshot.getChildren()) {
+                        week.getRef().removeValue();
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Required from interface implementation.
+            }
+        });
 
         Query query = db.getReference(TABLE_EMPLOYEES);
         query.addListenerForSingleValueEvent(new ValueEventListener() {
@@ -55,9 +73,9 @@ public class Schedule extends AppCompatActivity {
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 List<Employee> workingEmployeeList = new ArrayList<>();
                 if (dataSnapshot.exists()) {
-                    scheduleAndInputToDb(dataSnapshot, workingEmployeeList, true);
+                    getEmployeesFromDatabase(dataSnapshot, workingEmployeeList);
+                    scheduleAndInputToDb(workingEmployeeList);
                 }
-                scheduleAndInputToDb(null, workingEmployeeList, false);
             }
 
             @Override
@@ -67,21 +85,19 @@ public class Schedule extends AppCompatActivity {
         });
     }
 
-    private void scheduleAndInputToDb(DataSnapshot dataSnapshot, @NonNull List<Employee> workingEmployeeList, boolean isFirst) {
 
+    private void getEmployeesFromDatabase(DataSnapshot dataSnapshot, @NonNull List<Employee> workingEmployeeList) {
+        for (DataSnapshot entry : dataSnapshot.getChildren()) {
+            Employee employeePerson = new Employee(entry.getValue(Employee.class).getKodID(), entry.getValue(Employee.class).getName());
+            workingEmployeeList.add(employeePerson);
+        }
+    }
+
+    private void scheduleAndInputToDb(@NonNull List<Employee> workingEmployeeList) {
         Map<String, ScheduleHelper> scheduleMap = new HashMap<>();
-        if (isFirst) {
-            initMap(scheduleMap);
-            for (DataSnapshot entry : dataSnapshot.getChildren()) {
-                Employee employeePerson = entry.getValue(Employee.class);
 
-                prepareShifts(scheduleMap, employeePerson);
-                workingEmployeeList.add(employeePerson);
-            }
-            prepareAndInputData(1, scheduleMap.get(MORNING_SHIFT).getShiftNames(), scheduleMap.get(AFTERNOON_SHIFT).getShiftNames(), scheduleMap.get(NIGHT_SHIFT).getShiftNames());
-
-        } else {
-            for (int currentWeekNumber = 2; currentWeekNumber < NUMBER_OF_WEEKS + 1; currentWeekNumber++) {
+        for (int currentWeekNumber = 1; currentWeekNumber < NUMBER_OF_WEEKS + 1; currentWeekNumber++) {
+            for (int dayOfTheWeek = 0; dayOfTheWeek < 5; dayOfTheWeek++) {
                 initMap(scheduleMap);
                 Collections.sort(workingEmployeeList, new Comparator() {
                     @Override
@@ -91,13 +107,33 @@ public class Schedule extends AppCompatActivity {
                         return Integer.compare(p1.getHours(), p2.getHours());
                     }
                 });
+
                 Iterator<Employee> employeeIterator = workingEmployeeList.iterator();
+
                 while (employeeIterator.hasNext() && !allShiftsAreFull(scheduleMap)) {
                     Employee currentEmployee = employeeIterator.next();
                     prepareShifts(scheduleMap, currentEmployee);
                 }
-                prepareAndInputData(currentWeekNumber, scheduleMap.get(MORNING_SHIFT).getShiftNames(), scheduleMap.get(AFTERNOON_SHIFT).getShiftNames(), scheduleMap.get(NIGHT_SHIFT).getShiftNames());
+                prepareAndInputData(currentWeekNumber, dayOfTheWeek, scheduleMap.get(MORNING_SHIFT).getShiftNames(), scheduleMap.get(AFTERNOON_SHIFT).getShiftNames(), scheduleMap.get(NIGHT_SHIFT).getShiftNames());
             }
+        }
+        updateEmployeeHours(workingEmployeeList);
+    }
+
+    private void updateEmployeeHours(@NonNull List<Employee> workingEmployeeList) {
+
+        for (Employee employee : workingEmployeeList) {
+            FirebaseDatabase.getInstance().getReference()
+                    .child(TABLE_EMPLOYEES + "/" + employee.getKodID()).setValue(employee).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(Schedule.this, "Employees updated", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(Schedule.this, "Failed to update employees", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
         }
     }
 
@@ -127,35 +163,24 @@ public class Schedule extends AppCompatActivity {
         }
     }
 
-    private void prepareAndInputData(int currentWeekNumber, String morningShiftNames, String afternoonShiftNames, String nightShiftNames) {
+    private void prepareAndInputData(int currentWeekNumber, int dayNumber, String morningShiftNames, String afternoonShiftNames, String nightShiftNames) {
         morningShiftNames = StringUtils.substring(morningShiftNames, 1);
         afternoonShiftNames = StringUtils.substring(afternoonShiftNames, 1);
         nightShiftNames = StringUtils.substring(nightShiftNames, 1);
 
-        inputWeekDataToDb(currentWeekNumber, morningShiftNames, afternoonShiftNames, nightShiftNames);
+        inputWeekDataToDb(currentWeekNumber, dayNumber, morningShiftNames, afternoonShiftNames, nightShiftNames);
     }
 
-    private void inputWeekDataToDb(int currentWeekNumber, String morningShiftNames, String afternoonShiftNames, String nightShiftNames) {
-        for (int currentDayAsNumber = 0; currentDayAsNumber < 5; currentDayAsNumber++) {
-            DaySchedule currentDayMorning = new DaySchedule(morningShiftNames);
-            DaySchedule currentDayAfternoon = new DaySchedule(afternoonShiftNames);
-            DaySchedule currentDayNight = new DaySchedule(nightShiftNames);
+    private void inputWeekDataToDb(int currentWeekNumber, int currentDayAsNumber, String morningShiftNames, String afternoonShiftNames, String nightShiftNames) {
+        DaySchedule currentDayMorning = new DaySchedule(morningShiftNames);
+        DaySchedule currentDayAfternoon = new DaySchedule(afternoonShiftNames);
+        DaySchedule currentDayNight = new DaySchedule(nightShiftNames);
 
-            FirebaseDatabase.getInstance().getReference().child(StringUtils.join(TABLE_SCHEDULE, "/", WEEK_WEEK, currentWeekNumber, "/",
-                    DaysOfTheWeek.getDay(currentDayAsNumber), "/shift1")).setValue(currentDayMorning);
-            FirebaseDatabase.getInstance().getReference().child(StringUtils.join(TABLE_SCHEDULE, "/", WEEK_WEEK, currentWeekNumber, "/",
-                    DaysOfTheWeek.getDay(currentDayAsNumber), "/shift2")).setValue(currentDayAfternoon);
-            FirebaseDatabase.getInstance().getReference().child(StringUtils.join(TABLE_SCHEDULE, "/", WEEK_WEEK, currentWeekNumber, "/",
-                    DaysOfTheWeek.getDay(currentDayAsNumber), "/shift3")).setValue(currentDayNight).addOnCompleteListener(new OnCompleteListener<Void>() {
-                @Override
-                public void onComplete(@NonNull Task<Void> task) {
-                    if (task.isSuccessful()) {
-                        Toast.makeText(Schedule.this, "Job Done", Toast.LENGTH_LONG).show();
-                    } else {
-                        Toast.makeText(Schedule.this, "Failed", Toast.LENGTH_LONG).show();
-                    }
-                }
-            });
-        }
+        FirebaseDatabase.getInstance().getReference().child(StringUtils.join(TABLE_SCHEDULE, "/", WEEK_WEEK, currentWeekNumber, "/",
+                DaysOfTheWeek.getDay(currentDayAsNumber), "/shift1")).setValue(currentDayMorning);
+        FirebaseDatabase.getInstance().getReference().child(StringUtils.join(TABLE_SCHEDULE, "/", WEEK_WEEK, currentWeekNumber, "/",
+                DaysOfTheWeek.getDay(currentDayAsNumber), "/shift2")).setValue(currentDayAfternoon);
+        FirebaseDatabase.getInstance().getReference().child(StringUtils.join(TABLE_SCHEDULE, "/", WEEK_WEEK, currentWeekNumber, "/",
+                DaysOfTheWeek.getDay(currentDayAsNumber), "/shift3")).setValue(currentDayNight);
     }
 }
